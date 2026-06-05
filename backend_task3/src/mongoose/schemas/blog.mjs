8c595f8c28f993e55047e6a61bc1613501ef2fa9
p-k-0-1-7 <ballapremkumar.24.cse@anits.edu.in>
2026-06-05 22:55:45 +0530

    Create blog schema for mongoose

---
+++ backend_task3/src/mongoose/schemas/blog.mjs
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Blog = mongoose.model("Blog", blogSchema);
