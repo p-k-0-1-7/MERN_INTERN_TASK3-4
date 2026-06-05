8fe5145a27a7d5bfa68bacb6ce1013c67b2def87
p-k-0-1-7 <ballapremkumar.24.cse@anits.edu.in>
2026-06-05 22:53:22 +0530

    Refactor routing logic and cleanup imports in App.jsx

--- frontend_task3/src/App.jsx
+++ frontend_task3/src/App.jsx
@@ -1,34 +1,29 @@
-import {
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import BlogDashboard from "./pages/BlogDashboard";
 
 function App() {
-  const token =
-    localStorage.getItem("token");
-
   return (
     <Routes>
-      <Route
-        path="/login"
-        element={<Login />}
-      />
-
-      <Route
-        path="/signup"
-        element={<Signup />}
-      />
-
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route
         path="/"
         element={
-          token ? (
+          localStorage.getItem("token") ? (
             <Dashboard />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/blogs"
+        element={
+          localStorage.getItem("token") ? (
+            <BlogDashboard />
           ) : (
             <Navigate to="/login" />
           )
