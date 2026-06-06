3d84496f7a1f912251f0a4f7c44c0a6f8ae2eec2
p-k-0-1-7 <ballapremkumar.24.cse@anits.edu.in>
2026-06-06 14:53:50 +0530

    Update App.jsx

--- frontend_task3/src/App.jsx
+++ frontend_task3/src/App.jsx
@@ -4,28 +4,42 @@ import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import BlogDashboard from "./pages/BlogDashboard";
 
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" replace />;
+};
+
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/blogs"
+        element={
+          <ProtectedRoute>
+            <BlogDashboard />
+          </ProtectedRoute>
+        }
+      />
+
       <Route
         path="/"
         element={
           localStorage.getItem("token") ? (
-            <Dashboard />
+            <Navigate to="/dashboard" replace />
           ) : (
-            <Navigate to="/login" />
-          )
-        }
-      />
-      <Route
-        path="/blogs"
-        element={
-          localStorage.getItem("token") ? (
-            <BlogDashboard />
-          ) : (
-            <Navigate to="/login" />
+            <Navigate to="/login" replace />
           )
         }
       />
